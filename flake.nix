6b5363ba952c529002d08e3c5521f4727318b0fe
Neil Alen <neilaspace@outlook.com>
2026-04-17 22:22:36 +0800

    🚧 添加未实现的子命令

--- flake.nix
+++ flake.nix
@@ -1,76 +1,84 @@
-{
-  description = "Clericum - A Qt6 QML application";
+rec {
+    description = "To be filled";
 
-  inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs";
-    nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixpkgs-unstable&shallow=1";
-    #flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.url = "https://proxy.gitwarp.top/https://github.com/numtide/flake-utils/archive/refs/heads/master.zip";
-  };
+    inputs = {
+        #nixpkgs.url = "github:NixOS/nixpkgs";
+        nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixpkgs-unstable&shallow=1";
+        #flake-utils.url = "github:numtide/flake-utils";
+        flake-utils.url = "https://proxy.gitwarp.top/https://github.com/numtide/flake-utils/archive/refs/heads/master.zip";
+    };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        packages = rec {
-          default = clericum;
+    outputs =
+        {
+            self,
+            nixpkgs,
+            flake-utils,
+        }:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                name = "clericum";
+                version = "0.1.0";
+                homepage = "https://github.com/neila-a/${name}";
+                pkgs = import nixpkgs {
+                    inherit system;
+                };
+            in
+            {
+                packages = {
+                    default = pkgs.stdenv.mkDerivation {
+                        pname = name;
+                        version = version;
+                        src = ./.;
 
-          clericum = pkgs.stdenv.mkDerivation {
-            pname = "clericum";
-            version = "0.1";
-            src = ./.;
+                        nativeBuildInputs = with pkgs; [
+                            cmake
+                            ninja
+                            qt6.wrapQtAppsHook
+                        ];
 
-            nativeBuildInputs = with pkgs; [
-              cmake
-              ninja
-              qt6.wrapQtAppsHook
-            ];
+                        buildInputs = with pkgs; [
+                            qt6.qtbase
+                            qt6.qtdeclarative
+                        ];
 
-            buildInputs = with pkgs; [
-              qt6.qtbase
-              qt6.qtdeclarative
-            ];
+                        cmakeFlags = [
+                            "-DCMAKE_BUILD_TYPE=Release"
+                            "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
+                            "-DGLOBAL_PROJECT_NAME=${name}"
+                            "-DGLOBAL_PROJECT_VERSION=${version}"
+                            "-DGLOBAL_PROJECT_HOMEPAGE=${homepage}"
+                        ];
 
-            cmakeFlags = [
-              "-DCMAKE_BUILD_TYPE=Release"
-              "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
-            ];
+                        env = {
+                            GLOBAL_PROJECT_DESCRIPTION = description;
+                        };
 
-            installPhase = ''
-              runHook preInstall
-              ninja install
-              runHook postInstall
-            '';
+                        installPhase = ''
+                            runHook preInstall
+                            ninja install
+                            runHook postInstall
+                        '';
 
-          meta = {
-            homepage = "https://github.com/neila-a/clericum";
-            license = pkgs.lib.licenses.gpl3Plus;
-            platforms = pkgs.lib.platforms.linux;
-            mainProgram = "clericum";
-          };
-        };
-        };
+                        meta = {
+                            homepage = homepage;
+                            license = pkgs.lib.licenses.gpl3Plus;
+                            platforms = pkgs.lib.platforms.linux;
+                            mainProgram = name + "_app";
+                        };
+                    };
+                };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cmake
-            qt6.qtbase
-            qt6.qtdeclarative
-            ninja
-          ];
+                devShells.default = pkgs.mkShell {
+                    packages = with pkgs; [
+                        cmake
+                        qt6.qtbase
+                        qt6.qtdeclarative
+                        ninja
+                    ];
 
-          inputsFrom = [ self.packages.${system}.clericum ];
-        };
-      }
-    );
-
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org"
-    ];
-  };
+                    inputsFrom = [ self.packages.${system}.default ];
+                };
+            }
+        );
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,62 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QCommandLineParser>
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-                     &app, []() { QCoreApplication::exit(-1); },
-                     Qt::QueuedConnection);
-    engine.loadFromModule("clericum", "Main");
+    QCommandLineParser parser;
+    app.setApplicationName(_PROJECT_NAME);
+    app.setApplicationVersion(_PROJECT_VERSION);
+    parser.setApplicationDescription(_PROJECT_DESCRIPTION);
 
-    return QCoreApplication::exec();
+    parser.addPositionalArgument("action", "The action to do");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.process(app);
+    const QStringList arguments = parser.positionalArguments();
+    if (arguments.length() > 0) {
+        const QString action = arguments[0];
+        if (action.compare("gui") == 0) {
+            // gui 命令处理
+            QQmlApplicationEngine engine;
+            QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                &app, []() { QCoreApplication::exit(-1); },
+                Qt::QueuedConnection);
+            engine.loadFromModule(_PROJECT_NAME, "Main");
+
+            return QCoreApplication::exec();
+        } else if (action.compare("create") == 0) {
+            // create <path> 命令处理
+            if (arguments.length() < 2) {
+                parser.showHelp(-1);
+            }
+            const QString path = arguments[1];
+            // TODO: 实现 create 功能
+        } else if (action.compare("load") == 0) {
+            // load <store> <path> 命令处理
+            if (arguments.length() < 3) {
+                parser.showHelp(-1);
+            }
+            const QString store = arguments[1];
+            const QString path = arguments[2];
+            // TODO: 实现 load 功能
+        } else if (action.compare("unload") == 0) {
+            // unload <path> 命令处理
+            if (arguments.length() < 2) {
+                parser.showHelp(-1);
+            }
+            const QString path = arguments[1];
+            // TODO: 实现 unload 功能
+        } else if (action.compare("backup") == 0) {
+            // backup <path> 命令处理
+            if (arguments.length() < 2) {
+                parser.showHelp(-1);
+            }
+            const QString path = arguments[1];
+            // TODO: 实现 backup 功能
+        }
+    }
+    return 0;
 }
